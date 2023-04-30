2eed54e0bc667881758308822cc15cb0f6b06613
yskoht <ysk.oht@gmail.com>
2023-05-01 04:06:52 +0900

    npx create-ink-app --typescript chat-gpt-cli

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ chat-gpt-cli
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ chat-gpt-cli --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
