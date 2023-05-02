f252d8c445ddf29342d273e4419be84bdc3b4c83
yskoht <ysk.oht@gmail.com>
2023-05-03 04:35:52 +0900

    add isActive to MultiLineTextInput

--- source/components/MultilineTextInput/MultiLineTextInput.tsx
+++ source/components/MultilineTextInput/MultiLineTextInput.tsx
@@ -22,6 +22,7 @@ type Props = {
 	shouldSubmit?: (input: string, key: Key) => boolean;
 	showCursor?: boolean;
 	cursorColor?: string;
+	isActive?: boolean;
 };
 function MultiLineTextInput({
 	value,
@@ -30,53 +31,57 @@ function MultiLineTextInput({
 	shouldSubmit = keymap.hittingEnter,
 	showCursor = true,
 	cursorColor = 'gray',
+	isActive = true,
 }: Props) {
 	const [cursor, setCursor] = useState<Cursor>(0);
 
-	useInput((_input, key) => {
-		const input = replaceLineSep(_input);
+	useInput(
+		(_input, key) => {
+			const input = replaceLineSep(_input);
 
-		if (shouldSubmit(input, key)) {
-			onSubmit(value);
-			return;
-		}
+			if (shouldSubmit(input, key)) {
+				onSubmit(value);
+				return;
+			}
 
-		const {nextValue, nextCursor} = (() => {
-			if (keymap.shouldMoveUp(input, key)) {
-				return edit.moveUp(cursor, value);
-			}
-			if (keymap.shouldMoveDown(input, key)) {
-				return edit.moveDown(cursor, value);
-			}
-			if (keymap.shouldMoveLeft(input, key)) {
-				return edit.moveLeft(cursor, value);
-			}
-			if (keymap.shouldMoveRight(input, key)) {
-				return edit.moveRight(cursor, value);
-			}
-			if (keymap.shouldMoveHead(input, key)) {
-				return edit.moveHead(cursor, value);
-			}
-			if (keymap.shouldMoveTail(input, key)) {
-				return edit.moveTail(cursor, value);
-			}
-			if (keymap.shouldBackspace(input, key)) {
-				return edit.backspace(cursor, value);
-			}
-			if (keymap.shouldDelete(input, key)) {
-				return edit.del(cursor, value);
-			}
-			if (keymap.shouldKill(input, key)) {
-				return edit.kill(cursor, value);
-			}
-			return edit.insert(cursor, value, input);
-		})();
+			const {nextValue, nextCursor} = (() => {
+				if (keymap.shouldMoveUp(input, key)) {
+					return edit.moveUp(cursor, value);
+				}
+				if (keymap.shouldMoveDown(input, key)) {
+					return edit.moveDown(cursor, value);
+				}
+				if (keymap.shouldMoveLeft(input, key)) {
+					return edit.moveLeft(cursor, value);
+				}
+				if (keymap.shouldMoveRight(input, key)) {
+					return edit.moveRight(cursor, value);
+				}
+				if (keymap.shouldMoveHead(input, key)) {
+					return edit.moveHead(cursor, value);
+				}
+				if (keymap.shouldMoveTail(input, key)) {
+					return edit.moveTail(cursor, value);
+				}
+				if (keymap.shouldBackspace(input, key)) {
+					return edit.backspace(cursor, value);
+				}
+				if (keymap.shouldDelete(input, key)) {
+					return edit.del(cursor, value);
+				}
+				if (keymap.shouldKill(input, key)) {
+					return edit.kill(cursor, value);
+				}
+				return edit.insert(cursor, value, input);
+			})();
 
-		debug({input, key, cursor, value, nextValue, nextCursor});
+			debug({input, key, cursor, value, nextValue, nextCursor});
 
-		onChange(nextValue);
-		setCursor(nextCursor);
-	});
+			onChange(nextValue);
+			setCursor(nextCursor);
+		},
+		{isActive},
+	);
 
 	return (
 		<Lines
