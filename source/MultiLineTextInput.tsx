73d36c43d3eb8fd592347ca0aeecd884b5679a6c
yskoht <ysk.oht@gmail.com>
2023-05-02 21:31:01 +0900

    refactoring

--- source/MultiLineTextInput.tsx
+++ source/MultiLineTextInput.tsx
@@ -25,73 +25,72 @@ function toLines(value: string): string[] {
 	return value.split(LINE_SEP);
 }
 
+type Cursor = number;
 type Position = {
 	x: number;
 	y: number;
 };
-function toPosition(cursor: number, value: string): Position {
+function toPosition(cursor: Cursor, value: string): Position {
 	const lines = toLines(value.slice(0, cursor));
 	const y = Math.max(lines.length - 1, 0);
 	const x = lines[y]?.length ?? 0;
 	return {x, y};
 }
-
-function toCursor(x: number, y: number, value: string): number {
+function toCursor(x: number, y: number, value: string): Cursor {
 	const lines = toLines(value);
+	const len = (line: string) => line.length + 1; // for line sep
 	const cursor =
-		lines
-			.slice(0, y)
-			.reduce(
-				(acc, line) => acc + line.length + (y !== lines.length ? 1 : 0),
-				0,
-			) + x;
+		lines.slice(0, y).reduce((acc, line) => acc + len(line), 0) + x;
 	return cursor;
 }
 
-function moveUp(cursor: number, value: string): number {
+type Next = {
+	nextCursor: Cursor;
+	nextValue: string;
+};
+function moveUp(cursor: Cursor, value: string): Next {
 	const {x, y} = toPosition(cursor, value);
 	const lines = toLines(value);
 	const ny = Math.max(y - 1, 0);
 	const nx = Math.min(x, lines[ny]!.length);
 	const nextCursor = toCursor(nx, ny, value);
-	return nextCursor;
+	return {nextCursor, nextValue: value};
 }
-function moveDown(cursor: number, value: string): number {
+function moveDown(cursor: Cursor, value: string): Next {
 	const {x, y} = toPosition(cursor, value);
 	const lines = toLines(value);
 	const ny = Math.min(y + 1, lines.length - 1);
 	const nx = Math.min(x, lines[ny]!.length);
 	const nextCursor = toCursor(nx, ny, value);
-	return nextCursor;
+	return {nextCursor, nextValue: value};
 }
-function moveLeft(cursor: number, value: string): number {
+function moveLeft(cursor: Cursor, value: string): Next {
 	const {x, y} = toPosition(cursor, value);
 	const nx = Math.max(x - 1, 0);
 	const nextCursor = toCursor(nx, y, value);
-	return nextCursor;
+	return {nextCursor, nextValue: value};
 }
-function moveRight(cursor: number, value: string): number {
+function moveRight(cursor: Cursor, value: string): Next {
 	const {x, y} = toPosition(cursor, value);
 	const lines = toLines(value);
 	const nx = Math.min(x + 1, lines[y]!.length);
 	const nextCursor = toCursor(nx, y, value);
-	return nextCursor;
+	return {nextCursor, nextValue: value};
 }
-function moveHead(cursor: number, value: string): number {
+function moveHead(cursor: Cursor, value: string): Next {
 	const {y} = toPosition(cursor, value);
 	const nx = 0;
 	const nextCursor = toCursor(nx, y, value);
-	return nextCursor;
+	return {nextCursor, nextValue: value};
 }
-function moveTail(cursor: number, value: string): number {
+function moveTail(cursor: Cursor, value: string): Next {
 	const {y} = toPosition(cursor, value);
 	const lines = toLines(value);
 	const nx = lines[y]!.length;
 	const nextCursor = toCursor(nx, y, value);
-	return nextCursor;
+	return {nextCursor, nextValue: value};
 }
-
-function add(cursor: number, value: string, input: string) {
+function add(cursor: Cursor, value: string, input: string): Next {
 	const nextValue = value.slice(0, cursor) + input + value.slice(cursor);
 	const nextCursor = cursor + input.length;
 	return {
@@ -99,7 +98,7 @@ function add(cursor: number, value: string, input: string) {
 		nextValue,
 	};
 }
-function backspace(cursor: number, value: string) {
+function backspace(cursor: Cursor, value: string): Next {
 	const {x} = toPosition(cursor, value);
 	if (x === 0) {
 		return {nextCursor: cursor, nextValue: value};
@@ -112,7 +111,7 @@ function backspace(cursor: number, value: string) {
 		nextValue,
 	};
 }
-function del(cursor: number, value: string) {
+function del(cursor: Cursor, value: string): Next {
 	const {x, y} = toPosition(cursor, value);
 	const lines = toLines(value);
 	if (x === lines[y]!.length) {
@@ -127,13 +126,38 @@ function del(cursor: number, value: string) {
 	};
 }
 
-function TextInput({
+function shouldMoveUp(input: string, key: Key): boolean {
+	return key.upArrow || (key.ctrl && input === 'p');
+}
+function shouldMoveDown(input: string, key: Key): boolean {
+	return key.downArrow || (key.ctrl && input === 'n');
+}
+function shouldMoveLeft(input: string, key: Key): boolean {
+	return key.leftArrow || (key.ctrl && input === 'b');
+}
+function shouldMoveRight(input: string, key: Key): boolean {
+	return key.rightArrow || (key.ctrl && input === 'f');
+}
+function shouldMoveHead(input: string, key: Key): boolean {
+	return key.ctrl && input === 'a';
+}
+function shouldMoveTail(input: string, key: Key): boolean {
+	return key.ctrl && input === 'e';
+}
+function shouldBackspace(input: string, key: Key): boolean {
+	return key.backspace || (key.ctrl && input === 'h');
+}
+function shouldDelete(input: string, key: Key): boolean {
+	return key.delete || (key.ctrl && input === 'd');
+}
+
+function MultiLineTextInput({
 	value,
 	onChange,
 	onSubmit,
 	shouldSubmit = hittingEnter,
 }: Props) {
-	const [cursor, setCursor] = useState(0);
+	const [cursor, setCursor] = useState<Cursor>(0);
 
 	useInput((input, key) => {
 		if (shouldSubmit(input, key)) {
@@ -141,47 +165,36 @@ function TextInput({
 			return;
 		}
 
-		{
-			const nextCursor = (() => {
-				if (key.upArrow || (key.ctrl && input === 'p')) {
-					return moveUp(cursor, value);
-				}
-				if (key.downArrow || (key.ctrl && input === 'n')) {
-					return moveDown(cursor, value);
-				}
-				if (key.leftArrow || (key.ctrl && input === 'b')) {
-					return moveLeft(cursor, value);
-				}
-				if (key.rightArrow || (key.ctrl && input === 'f')) {
-					return moveRight(cursor, value);
-				}
-				if (key.ctrl && input === 'a') {
-					return moveHead(cursor, value);
-				}
-				if (key.ctrl && input === 'e') {
-					return moveTail(cursor, value);
-				}
-				return null;
-			})();
-			if (nextCursor !== null) {
-				setCursor(nextCursor);
-				return;
+		const {nextValue, nextCursor} = (() => {
+			if (shouldMoveUp(input, key)) {
+				return moveUp(cursor, value);
 			}
-		}
+			if (shouldMoveDown(input, key)) {
+				return moveDown(cursor, value);
+			}
+			if (shouldMoveLeft(input, key)) {
+				return moveLeft(cursor, value);
+			}
+			if (shouldMoveRight(input, key)) {
+				return moveRight(cursor, value);
+			}
+			if (shouldMoveHead(input, key)) {
+				return moveHead(cursor, value);
+			}
+			if (shouldMoveTail(input, key)) {
+				return moveTail(cursor, value);
+			}
+			if (shouldBackspace(input, key)) {
+				return backspace(cursor, value);
+			}
+			if (shouldDelete(input, key)) {
+				return del(cursor, value);
+			}
+			return add(cursor, value, input);
+		})();
 
-		{
-			const {nextValue, nextCursor} = (() => {
-				if (key.backspace || (key.ctrl && input === 'h')) {
-					return backspace(cursor, value);
-				}
-				if (key.delete || (key.ctrl && input === 'd')) {
-					return del(cursor, value);
-				}
-				return add(cursor, value, input);
-			})();
-			onChange(nextValue);
-			setCursor(nextCursor);
-		}
+		onChange(nextValue);
+		setCursor(nextCursor);
 	});
 
 	const {x: cx, y: cy} = useMemo(
@@ -212,4 +225,4 @@ function TextInput({
 	return <>{texts}</>;
 }
 
-export default TextInput;
+export default MultiLineTextInput;
