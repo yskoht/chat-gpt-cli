f0d0d024461fcd9dcfe8279e441b3b436582f6fc
yskoht <ysk.oht@gmail.com>
2023-05-02 22:27:30 +0900

    remove shouldSubmit

--- source/Chat.tsx
+++ source/Chat.tsx
@@ -1,29 +1,12 @@
-import React, {useState, useCallback, useRef} from 'react';
+import React, {useState, useCallback} from 'react';
 import MultiLineTextInput from './MultiLineTextInput.js';
-import {Key} from 'ink';
 
 function Chat() {
 	const [text, setText] = useState('');
 	const onSubmit = useCallback(() => setText(''), []);
-	const returnCount = useRef(0);
-
-	const hittingEnterTwice = useCallback((_input: string, key: Key) => {
-		if (key.return) {
-			returnCount.current = (returnCount.current + 1) % 2;
-			return returnCount.current === 0;
-		}
-
-		returnCount.current = 0;
-		return false;
-	}, []);
 
 	return (
-		<MultiLineTextInput
-			value={text}
-			onChange={setText}
-			onSubmit={onSubmit}
-			shouldSubmit={hittingEnterTwice}
-		/>
+		<MultiLineTextInput value={text} onChange={setText} onSubmit={onSubmit} />
 	);
 }
 
