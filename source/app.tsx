a3f04299537934416b76bcbf02a4b672c76233de
yskoht <ysk.oht@gmail.com>
2023-05-03 16:40:41 +0900

    fix layout

--- source/app.tsx
+++ source/app.tsx
@@ -3,14 +3,22 @@ import React from 'react';
 
 import Chat from '@/components/Chat/index.js';
 import ChatRecord from '@/components/ChatRecord/index.js';
+import Divider from '@/components/Divider/index.js';
 import NewChat from '@/components/NewChat/index.js';
 import useController from '@/hooks/useController.js';
+import useDimension from '@/hooks/useDimension.js';
 
 export default function App() {
+	const {width, height} = useDimension();
 	useController();
 
 	return (
-		<Box flexDirection="row" justifyContent="space-between">
+		<Box
+			flexDirection="row"
+			justifyContent="space-between"
+			width={width}
+			height={height}
+		>
 			<Box
 				flexDirection="column"
 				justifyContent="flex-end"
@@ -22,11 +30,10 @@ export default function App() {
 				<Chat />
 			</Box>
 
-			<Box flexDirection="column" width="20%">
-				<Box flexDirection="column" borderStyle="single">
-					<NewChat />
-				</Box>
-				<Box flexDirection="column" borderStyle="single">
+			<Box flexDirection="column" width="20%" borderStyle="single">
+				<NewChat />
+				<Divider paddingLeft={1} paddingRight={1} />
+				<Box flexDirection="column">
 					<ChatRecord label="Hello1" />
 					<ChatRecord label="Hello2" />
 					<ChatRecord label="Hello3" />
