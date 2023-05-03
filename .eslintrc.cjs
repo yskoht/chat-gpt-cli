4ea6f30e70b5e57e9f4e651dbfeb8079e1783b0e
yskoht <ysk.oht@gmail.com>
2023-05-03 10:01:38 +0900

    fix .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'react-hooks'],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'react-hooks/exhaustive-deps': 'error',
+	},
+};
