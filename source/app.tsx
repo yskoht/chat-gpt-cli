7c32f49e311866329b25e043d8e277613561016e
yskoht <ysk.oht@gmail.com>
2023-05-14 17:10:33 +0900

    fix app

--- source/app.tsx
+++ source/app.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Chat from '@/components/Chat/index.js';
 import Menu from '@/components/Menu/index.js';
+import StatusBar from '@/components/StatusBar/index.js';
 import useChatRecord from '@/hooks/useChatRecord.js';
 import useDimension from '@/hooks/useDimension.js';
 import useFocusManagement from '@/hooks/useFocusManagement.js';
@@ -20,14 +21,18 @@ export default function App() {
 	const id = useChatRecord(({id}) => id);
 
 	return (
-		<Box justifyContent="space-between" width={width} height={height - 1}>
-			<Box width={CHAT_WIDTH}>
-				<Chat id={id} />
+		<Box flexDirection="column">
+			<Box width={width} height={height - 1}>
+				<Box width={CHAT_WIDTH}>
+					<Chat id={id} />
+				</Box>
+
+				<Box width={MENU_WIDTH}>
+					<Menu id={id} />
+				</Box>
 			</Box>
 
-			<Box width={MENU_WIDTH}>
-				<Menu id={id} />
-			</Box>
+			<StatusBar />
 		</Box>
 	);
 }
